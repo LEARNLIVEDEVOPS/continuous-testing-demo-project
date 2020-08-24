f440239729b68c50b3e4d077fb2dc730a938076c
LEARNLIVEDEVOPS <68138803+LEARNLIVEDEVOPS@users.noreply.github.com>
2020-08-24 22:33:06 +0530

    Update ExceptionThrower.java

--- src/test/java/com/tcsiondemo/utilities/ExceptionThrower.java
+++ src/test/java/com/tcsiondemo/utilities/ExceptionThrower.java
@@ -1,8 +1,8 @@
-package com.edurekademo.utilities;
+package com.tcsiondemo.utilities;
 
 import java.io.IOException;
 
-import com.edurekademo.utilities.LoggerStackTraceUtil;
+import com.tcsiondemo.utilities.LoggerStackTraceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
