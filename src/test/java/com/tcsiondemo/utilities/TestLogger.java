93245853b301e38f8050b57f428d9297c014d12b
LEARNLIVEDEVOPS <68138803+LEARNLIVEDEVOPS@users.noreply.github.com>
2020-08-24 22:57:19 +0530

    Update TestLogger.java

--- src/test/java/com/tcsiondemo/utilities/TestLogger.java
+++ src/test/java/com/tcsiondemo/utilities/TestLogger.java
@@ -1,5 +1,5 @@
-package com.edurekademo.utilities;
-import com.edurekademo.utilities.LoggerStackTraceUtil;
+package com.tcsiondemo.utilities;
+import com.tcsiondemo.utilities.LoggerStackTraceUtil;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
